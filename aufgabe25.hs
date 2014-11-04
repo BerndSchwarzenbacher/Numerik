712fcd32580d5b232a0a66444ad2fa472d2a20d2
Bernd Schwarzenbacher <bernd.schwb@yahoo.com>
2014-11-04 01:08:42 +0100

    aufgabe 25

--- aufgabe25.hs
+++ aufgabe25.hs
@@ -1,9 +1,9 @@
 aitken :: (Double -> Double) -> [Double] -> Int -> Int -> Double
-aitken f h n k
-    | k >= n + (length h) = -1
-    | n == 0 = f $ h !! k
-    | otherwise = let akm1 = aitken f h (n-1) k
-                      ak1m1 = aitken f h (n-1) (k-1)
-                      hkm = h !! (k+n)
-                      hk = h !! k
-                  in akm1 + (akm1 - ak1m1) / ((hkm/hk) - 1.0) 
+aitken f h n j
+    -- | j >= n + k = Nothing
+    | n == 0 = f $ h !! j
+    | otherwise = let ajm1 = aitken f h (n-1) j
+                      aj1m1 = aitken f h (n-1) (j+1)
+                      hjm = h !! (j+n)
+                      hj = h !! j
+                  in ajm1 + (ajm1 - aj1m1) / ((hjm/hj) - 1.0)
